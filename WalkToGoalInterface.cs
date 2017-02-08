db3a2484b4d7f2663073fc7d48d10e848cf27cac
mariusrubo <marius.rubo@uni-wuerzburg.de>
2017-02-08 15:58:38 +0100

    Update and rename WalkToCall.cs to WalkToGoalInterface.cs

--- WalkToGoalInterface.cs
+++ WalkToGoalInterface.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WalkToCall : MonoBehaviour {
+public class WalkToGoalInterface : MonoBehaviour {
 
     Transform CurrentGoal;
     [Tooltip("Manually drag goal 1 here.")]
